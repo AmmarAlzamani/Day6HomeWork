9bf734d9132c2c665cb2116cd9242cb5027019c3
ammaralzamani <ammaralzamani@ammars-mbp.fios-router.home>
2021-12-21 20:29:13 -0500

    day 6 homework

--- src/homework/questionOne.java
+++ src/homework/questionOne.java
@@ -1,5 +1,43 @@
 package homework;
 
 public class questionOne {
-
+	public static void main(String[] args) {
+		
+		String colorOption = "orange";
+		
+		switch (colorOption){
+		case "red":
+			System.out.println("red color");
+			break;
+			
+		case "orange":
+			System.out.println("orange color");
+			break;
+			
+		case "black":
+			System.out.println("black color");
+			break;
+			
+		default:
+			System.out.println("wrong color, not available");
+			break;
+			
+			
+		}
+		
+		if (colorOption == "blue") {
+			System.out.println("blue color");
+			
+		}else if(colorOption == "orange") {
+			System.out.println("orange color");
+		
+		}else if(colorOption == "black") {
+			System.out.println("black color");
+			
+		}else {
+			System.out.println("wrong color, not available");
+		}
+		
+	
+}
 }
--- src/homework/questionTwo.java
+++ src/homework/questionTwo.java
@@ -1,5 +1,47 @@
 package homework;
 
 public class questionTwo {
+	public static void main(String[] args) {
+		
+		char gender = 'm';
+		
+		switch (gender) {
+			case 'm':
+				System.out.println("Male");
+				break;
+				
+			case 'f':
+				System.out.println("Female");
+				break;
+				
+			case'F':
+				System.out.println("Female");
+				break;
+				
+			case 'M':
+				System.out.println("Male");
+				break;
+				
+				default: 
+					System.out.println("not a gender");
+					break;
+		}
+		
+		if(gender == 'm') {
+			System.out.println("Male");
+			
+		}else if (gender == 'f') {
+			System.out.println("Female");
+			
+		}else if (gender == 'F') {
+			System.out.println("Female");
+			
+		}else if (gender == 'M') {
+			System.out.println("Male");
+			
+		}else {
+			System.out.println("not a gender");
+		}
+	}
 
 }
